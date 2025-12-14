341058d4ac33cf4554e738807de7d1b4cfb7200a
Waldemberg <waldemberg.pereira@outlook.com>
2025-12-14 01:30:16 -0300

    redirecionado o usuário para a pagina inicial quando tenta acessar a raiz da aplicação

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tarefas/', include('tarefas.urls')),
-    path('core/', include('core.urls'))
+    path('core/', include('core.urls')),
+    path('', RedirectView.as_view(pattern_name='login', permanent=True))
 ]
