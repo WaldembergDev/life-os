8390463b6781fa9fcbb224c38b95c642ad1cb5d3
Waldemberg <waldemberg.pereira@outlook.com>
2026-02-15 03:59:23 -0300

    adicionado o atalho entretenimentos na navbar

--- entretenimentos/urls.py
+++ entretenimentos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('novo-entretenimento/', views.entretenimento_create, name='entretenimento_create'),
+    path('', views.entretenimento_list, name='entretenimento_list')
 ]
