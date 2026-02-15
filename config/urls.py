f29a65b6f355ac563e2402084e3fcf9c05529166
Waldemberg <waldemberg.pereira@outlook.com>
2026-02-15 10:56:40 -0300

    Efetuado a configuração para direcionar o usuário para a página inicial quando tenta acessar a página de login autenticado

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
 from two_factor.urls import urlpatterns as tf_urls
+from two_factor.views import LoginView
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='two_factor:login', permanent=True)),
+    path('account/login/', LoginView.as_view(redirect_authenticated_user=True), name='two_factor:login'),
+    path('', include(tf_urls)),
     path('admin/', admin.site.urls),
     path('tarefas/', include('tarefas.urls')),
     path('core/', include('core.urls')),    
-    path('', include(tf_urls)),
-    path('entretenimentos/', include('entretenimentos.urls'))
+    path('entretenimentos/', include('entretenimentos.urls')),
+    path('', RedirectView.as_view(pattern_name='pagina_inicial', permanent=False)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,30 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth import login as as_login, logout as as_logout, authenticate
-from .forms import LoginForm
+from django.contrib.auth import logout as as_logout
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-# def login(request):
-#     if request.user.is_authenticated:
-#         return redirect('pagina_inicial')
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             email = form.cleaned_data.get('email')
-#             password = form.cleaned_data.get('senha')
-#             user = authenticate(request, email=email, password=password)
-#             if user:
-#                 as_login(request, user)
-#                 return redirect('pagina_inicial')
-#             else:
-#                 messages.error(request, 'Login ou senha inválidos!')
-#     else:
-#         form = LoginForm()
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'core/login.html', context)
 
 
 @login_required
