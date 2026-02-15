2820db4cbf2c2da9ab96f592f5b6c373c52d1600
Waldemberg <waldemberg.pereira@outlook.com>
2026-02-15 03:55:13 -0300

    Desenvolvido a view entretenimento_list

--- entretenimentos/views.py
+++ entretenimentos/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Entretenimento
+from .forms import EntretenimentoForm
+from django.contrib import messages
 
 # Create your views here.
 def entretenimento_create(request):
     """ view que cria um entretenimento """
     if request.method == 'POST':
-        pass
+        form = EntretenimentoForm(request.POST)
+        if form.is_valid():
+            entretenimento = form.save(commit=False)
+            entretenimento.criado_por = request.user
+            entretenimento.save()
+            messages.success(request, 'Entretenimento criado com sucesso!')
+            return redirect('entretenimento_create')
     else:
-        pass
+        form = EntretenimentoForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'entretenimentos/entretenimento_create.html', context)
+
+def entretenimento_list(request):
+    """ view que exibe os entrenimentos do sistema """
+    entretenimentos = Entretenimento.objects.all()
+    context = {
+        'entretenimentos': entretenimentos
+    }
+    return render(request, 'entretenimentos/entretenimento_list.html', context)
